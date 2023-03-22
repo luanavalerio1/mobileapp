d9609a0317f58c333fd812d806c0e58a7d39b1d6
luanavalerio1 <luanavallerio@gmail.com>
2023-03-22 16:46:17 -0300

    novo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { Dispatch, SetStateAction, useState} from 'react';
+import { 
+  ScreensSlider1, ScreensSlider2, ScreensSlider3, ScreensSlider4,
+} from "./src/screens" 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>oi!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [page, setPage] = useState(1)
+  switch (page) {
+    case 1: 
+    return <ScreensSlider1 setPageI={setPage} />
+    break;
+    case 2: 
+    return <ScreensSlider2 setPageI={setPage} />
+    break;
+    case 3: 
+    return <ScreensSlider3 setPageI={setPage} />
+    break;
+    case 4: 
+    return <ScreensSlider4 setPageI={setPage} />
+    break;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
