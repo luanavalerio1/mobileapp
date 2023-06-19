4f687b218db243dae1078093d936273a6f449e17
luanavalerio1 <luanavallerio@gmail.com>
2023-06-19 12:14:01 -0300

    direcion

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: ['react-native-reanimated/plugin',
+      ['modules:react-native-dotenv', {
+        "moduleName": "@env",
+        "path": false,
+        "allowUndefined": true
+      }]]
   };
 };
--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Platform, Text, View, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
-import MapView,{Region, Marker} from 'react-native-maps';
-import {styles} from "./styles"
+import MapView, { Region, Marker, Polyline } from 'react-native-maps';
+import {API_GOOGLE} from '@env'
+import { styles } from "./styles"
+import { colors } from '../../styles/colors';
+import {GooglePlacesAutocomplete, GooglePlaceData, GooglePlaceDetail} from 'react-native-google-places-autocomplete'
+import MapViewDirections from 'react-native-maps-directions'
 
-export  function LocationScreen() {
+type ICoords = {
+  latitude: number
+  longitude: number
+}
+
+export function LocationScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [region, setRegion] = useState<Region>()
   const [marker, setMarker] = useState<Region[]>()
-
+  const [coords, setCoords] = useState<ICoords[]>([])
 
   useEffect(() => {
+    let subscription: Location.LocationSubscription
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -28,13 +38,26 @@ export  function LocationScreen() {
         longitudeDelta: 0.006
       })
       setMarker([{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.006,
-            longitudeDelta: 0.006
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.006,
+        longitudeDelta: 0.006
       }])
+      subscription = await Location.watchPositionAsync({
+        accuracy: Location.LocationAccuracy.High,
+        timeInterval: 1000,
+        distanceInterval: 1
+      }, (location) => {
+        setCoords((prevState) => [...prevState, location.coords])
+      });
     })();
-  }, []);
+
+    return () => {
+      if (subscription) {
+        subscription.remove()
+      }
+    }
+  }, [])
 
   let text = 'Waiting..';
   if (errorMsg) {
@@ -45,18 +68,23 @@ export  function LocationScreen() {
 
   return (
     <View style={styles.container}>
-        {region ? (
-            <MapView region={region} style={styles.map} >
-              {marker && marker.map ((i) => (
-                <Marker key={i.latitude} coordinate={i} />
-              ))}
-            </MapView>
-        ):(
-            <Text style={styles.paragraph}>{text}</Text>
+      {region ? (
+        <MapView region={region} style={styles.map} showsUserLocation={true} >
+          {marker && marker.map((i) => (
+            <Marker key={i.latitude} coordinate={i} />
+          ))}
+          {coords && <Polyline
+            coordinates={coords}
+            strokeColor={colors.fourth}
+            strokeWidth={7}
+          />}
+        </MapView>
+      ) : (
+        <Text style={styles.paragraph}>{text}</Text>
 
-        )}
-        
-     
+      )}
+
+
     </View>
   );
 }
