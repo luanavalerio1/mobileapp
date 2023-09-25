5e87cdc1de06a5728214a4c8f77267082eee9e19
luanavalerio1 <luanavallerio@gmail.com>
2023-09-25 08:31:02 -0300

    foi

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -3,8 +3,22 @@ import { View, Text } from "react-native"
 import { ComponentButtonInterface } from "../../components"
 import { TabTypes } from "../../navigations/tab.navigation"
 import { styles } from "./styles"
+import * as Notifications from 'expo-notifications';
+import {registerForPushNotificationsAsync} from "../../services/data/Push";
+import {useAuth} from "../../hooks/auth";
+import {useEffect, useState} from "react"
+import { ComponentLoading } from "../../components"
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        sh
+    })
+}
+   
+)
 
 export function Perfil({ navigation }: TabTypes) {
+    const {user} = useAuth()
+
     function handleVoltar() {
         const tab = navigation.getParent()
         tab?.goBack()
